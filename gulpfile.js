c4ef1d9b79a4446ef4c21fd91baece6b3c4e7ef3
Greg <greg@elmnt.com>
2017-03-10 06:12:08 -0800

    Cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var gulp         = require('gulp'),
     browserSync  = require('browser-sync').create(),
     concat       = require('gulp-concat'),
     uglify       = require('gulp-uglify'),
+    babel        = require('gulp-babel'),
     rename       = require('gulp-rename'),
     sass         = require('gulp-sass'),
     maps         = require('gulp-sourcemaps'),
     cache        = require('gulp-cache'),
-    del          = require('del'),
-    babel        = require('gulp-babel');
+    del          = require('del');
 
 // ----------- Server
 
@@ -21,30 +21,28 @@ gulp.task('serve', ['minifyScripts', 'compileSass'], function() {
         server: "./app"
     });
 
-    // Watch styles, scripts, snippets/templates, kirby content text files
+    // watch
     gulp.watch('app/src/styles/**/*.scss', ['compileSass']);
     gulp.watch('app/src/js/*', ['minifyScripts']);
     gulp.watch('app/*.html').on('change', browserSync.reload);
-    //gulp.watch('app/*.php').on('change', browserSync.reload);
-    //gulp.watch('app/inc/**/*.php').on('change', browserSync.reload);
 
 });
 
-// ----------- Server & Watch Files (PHP setup)
+// ----------- Server & Watch Files (PHP/MAMP setup)
 //
 // gulp.task('serve', ['minifyScripts', 'compileSass'], function() {
 //
-//     // Note: I'm using MAMP PRO for my local server
-//     browserSync.init({
-//         proxy: 'localhost:8888',
-//         notify: false
-//     });
+//  // Note: I'm using MAMP PRO for my local server
+//  browserSync.init({
+//      proxy: 'localhost:8888',
+//      notify: false
+//  });
 //
-//     // Watch styles, scripts, snippets/templates, kirby content text files
-//     gulp.watch('app/src/styles/**/*.scss', ['compileSass']);
-//     gulp.watch('app/src/js/*', ['minifyScripts']);
-//     gulp.watch('app/*.php').on('change', browserSync.reload);
-//     gulp.watch('app/inc/**/*.php').on('change', browserSync.reload);
+//  // watch
+//  gulp.watch('app/src/styles/**/*.scss', ['compileSass']);
+//  gulp.watch('app/src/js/*', ['minifyScripts']);
+//  gulp.watch('app/*.php').on('change', browserSync.reload);
+//  gulp.watch('app/inc/**/*.php').on('change', browserSync.reload);
 //
 // });
 
@@ -58,7 +56,6 @@ gulp.task('concatScripts', function() {
     .pipe(gulp.dest('app/src/js/'));
 });
 
-// Now minify it, and put it in assets/js/
 gulp.task('minifyScripts', ['concatScripts'], function() {
     return gulp.src('app/src/js/app.js')
         .pipe(maps.init())
@@ -90,18 +87,13 @@ gulp.task('compileSass', function () {
 // ----------- Cleanup
 
 gulp.task('clean', function(callback) {
-
     del('app/css/*.css*');
     del('app/js/*.js*');
-
     del('app/src/js/app.js');
     del('app/src/js/app.min.js');
     del('app/src/js/app.min.js.map');
-
     del('dist');
-
     return cache.clearAll(callback);
-
 })
 
 // ----------- Build
